0376d5952d78abb1e0e9eb6f6fc1aedb976893da
gbrlhnz <gabrielhansley.suarez@sun-asterisk.com>
2022-07-19 16:25:19 +0800

    Fixed route conflicts

--- elearning-backend/routes/api.php
+++ elearning-backend/routes/api.php
@@ -22,6 +22,7 @@ Route::post('/login', [SessionsController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     // Authenticated Routes
     Route::post('/user', [UserProfileController::class, 'index']);
+    Route::post('/update-user', [UserController::class, 'update']);
     Route::post('/logout', [SessionsController::class, 'logout']);
 
     // Admin Routes
